a091e5c7dcc61cb3710868add9f2053b2fd7b602
IamEinstein1 <vermarishit039@gmail.com>
2020-10-22 18:21:23 +0530

    generates a random id everytime and adds it to a file(id.txt)

--- main/id.py
+++ main/id.py
@@ -1,9 +1,20 @@
 import uuid
-file = open('id.txt', mode='a')
+
+
+# def create_id():
+#     file = open('random.txt', mode='w', newline='')
+#     id = uuid.uuid4()
+#     file.write(str(id)+'\n')
+#     file.close()
+#     return id
 
 
 def create_id():
-    file.write('YY')
     id = uuid.uuid4()
+    print(id)
+    file = open("id.txt", mode="a", newline='', encoding='UTF-8')
+    print(file)
+    # check = open("random.txt", mode="r", newline='')
     file.write(str(id)+'\n')
+    file.close()
     return id
