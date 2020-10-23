3c475e36659a3a870ea4d7c459e00970809fbf6e
IamEinstein1 <vermarishit039@gmail.com>
2020-10-23 21:24:58 +0530

    made required changed

--- main/db.py
+++ main/db.py
@@ -1,19 +1,14 @@
 
 # * made this file to make database functions  that will work upon the mail
-# !BETTER CoMMENTS TO BE USED DOWNLOAD IT HERE -> https://marketplace.visualstudio.com/items?itemName=aaron-bond.better-comments
-
-def created_time(id):
-    import datetime
-    now = datetime.datetime.now()
-    id.time_created = now
-    id.save()
-    return "Success"
+#!BETTER CoMMENTS TO BE USED DOWNLOAD IT HERE -> https://marketplace.visualstudio.com/items?itemName=aaron-bond.better-comments
 
 
-def check_if_key_is_valid(id, objs):
-    import datetime
-    created = id.time_created
-    then = datetime.datetime(2020, 10, 23, 17)
-    dif = then-created
-    if dif > datetime.timedelta(hours=2):
-        objs.objects.delete(pk=id.id)
+def check_if_key_is_valid(ids):
+    from datetime import datetime, timedelta, timezone
+    for id in ids.objects.all():
+        if datetime.now(tz=timezone.utc) - id.time_created > timedelta(hours=2):
+            ids.objects.delete(id)
+            return False
+        else:
+            print("ID IS VALID")
+            return True
--- main/views.py
+++ main/views.py
@@ -1,24 +1,42 @@
+# from datetime import datetime
 from django.http.response import HttpResponse
 from django.shortcuts import render, redirect
 from .mail import *
 from .db import *
 from .models import *
-
-import uuid
+from django.db.utils import IntegrityError
 
 
 def index(request):
+    check_if_key_is_valid(Id)
     return render(request, 'main/index.html')
 
 
 def mail(request):
+    check_if_key_is_valid(Id)
     if request.method == "POST":
         if verifymail(request.POST['mail']):
+            # To get the location
+            ip = request.META.get('REMOTE_ADDR', None)
+
             import uuid
-            id = uuid.uuid4()
-            print(request.build_absolute_uri())
-            print(request.get_full_path())
-            return redirect('mail:id', id)
+            id = Id.objects.create()
+            id.unique_id = uuid.uuid4()
+            id.ip = ip
+            # id.time_created = datetime.now()
+            mail = request.POST['mail']
+            for item in Id.objects.all():
+                if item.mail == mail:
+                    return render(request, 'main/index.html', context={"error": "You have already registered"})
+                else:
+                    pass
+
+            try:
+                id.mail = mail
+            except IntegrityError:
+                return render(request, 'main/index.html', context={"error": "You have already registered"})
+            id.save()
+            return redirect('mail:id', id.unique_id)
         else:
             return render(request, 'main/index.html', context={"error": "Provide a valid mail"})
     else:
@@ -26,12 +44,8 @@ def mail(request):
 
 
 def id(request, id):
-    file = open('id.txt', mode='r')
-    if str(id)+'\n' in file.readlines():
-        file.close()
-        return render(request, 'main/id.html', context={"id": "Session expired"})
-    else:
-        file = open("id.txt", mode="a", newline='', encoding='UTF-8')
-        file.write(str(id)+'\n')
-        file.close()
+    check_if_key_is_valid(Id)
+    try:
         return render(request, 'main/id.html', context={"id": id})
+    except (Id.DoesNotExist, KeyError):
+        return render(request, 'main/id.html', context={"id": "Session Expireds"})
