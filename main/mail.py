dc79a7f1c5876077cfe995927aa2816dade4367a
Rishit <63300310+IamEinstein1@users.noreply.github.com>
2020-10-28 17:35:52 +0530

    Update mail.py

--- main/mail.py
+++ main/mail.py
@@ -8,10 +8,6 @@ def verifymail(mail):
 
 
 def send__mail(mail):
-    EMAIL_HOST = 'smtp.mailtrap.io'
-    EMAIL_HOST_PASSWORD = '407f4156f197b3'
-    EMAIL_HOST_USER = '9b4c1049781f9b'
-    EMAIL_PORT = '2525'
     from django.core.mail import send_mail
     print(send_mail("VERIFY YOUR ID", "CONTENT", recipient_list=[
         "vermarishit039@gmail.com "]))
